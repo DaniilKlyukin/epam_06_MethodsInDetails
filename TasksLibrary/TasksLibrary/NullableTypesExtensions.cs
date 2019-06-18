f83fde8d8e6a267c84ad42af9b7591dbaaaf6ed3
Daniil <anatoliikljukin@mail.ru>
2019-06-18 22:00:19 +0400

    Create NullableTypesExtensions.cs
    Solved 3rd task (extension method IsNull for null-able type)

---
+++ TasksLibrary/TasksLibrary/NullableTypesExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TasksLibrary
+{
+    public static class NullableTypesExtensions
+    {
+        public static bool IsNull<T>(this T? input) where T : struct
+        {
+            return input == null;
+        }
+
+        public static bool IsNull<T>(this T input) where T : class
+        {
+            return input == null;
+        }
+    }
+}

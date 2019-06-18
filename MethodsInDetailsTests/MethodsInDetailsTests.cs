ae85bb4d5b0dfa4cca1ba989ff4837dc5c0fb4dd
Daniil <anatoliikljukin@mail.ru>
2019-06-18 21:59:09 +0400

    Update MethodsInDetailsTests.cs
    Added tests for 3rd task (null-able types).

--- MethodsInDetailsTests/MethodsInDetailsTests.cs
+++ MethodsInDetailsTests/MethodsInDetailsTests.cs
@@ -1,6 +1,8 @@
 ﻿using TasksLibrary;
 using NUnit.Framework;
 using MSUnitTest = Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections;
 
 namespace MethodsInDetailsTests
 {
@@ -54,6 +56,34 @@ namespace MethodsInDetailsTests
         {
             double workTime;
             return StainGreatestCommonDivisor.GetGCD(out workTime, num1, num2, numbers);
+        }
+
+        [MSUnitTest.TestMethod]
+        public void CheckIsNull()
+        {
+            int? i1 = 1;
+            Assert.AreEqual(false, i1.IsNull());
+
+            int? i2 = null;
+            Assert.AreEqual(true, i2.IsNull());
+
+            string s1 = null;
+            Assert.AreEqual(true, s1.IsNull());
+
+            string s2 = "Some str";
+            Assert.AreEqual(false, s2.IsNull());
+
+            object o1 = "boxing";
+            Assert.AreEqual(false, o1.IsNull());
+
+            object o2 = null;
+            Assert.AreEqual(true, o2.IsNull());
+
+            int[] arr1 = null;
+            Assert.AreEqual(true, arr1.IsNull());
+
+            int[] arr2 = { 1, 2, 3 };
+            Assert.AreEqual(false, arr2.IsNull());
         }
     }
 }

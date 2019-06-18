514b052ad957ca6528c328d4df2a43d39e4b199b
Daniil <anatoliikljukin@mail.ru>
2019-06-18 15:07:30 +0400

    Update MethodsInDetailsTests.cs
    Added tests for 2nd task.

--- MethodsInDetailsTests/MethodsInDetailsTests.cs
+++ MethodsInDetailsTests/MethodsInDetailsTests.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using TasksLibrary;
+﻿using TasksLibrary;
 using NUnit.Framework;
 using MSUnitTest = Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -27,6 +26,19 @@ namespace MethodsInDetailsTests
         public string CheckToIEEE754(double number)
         {
             return number.ToIEEE754();
+        }
+
+        [TestCase(10, 15, 20, ExpectedResult = 5)]
+        [TestCase(-5, 10, ExpectedResult = 5)]
+        [TestCase(1, 1, ExpectedResult = 1)]
+        [TestCase(0, 140, 20, -50, 13, ExpectedResult = 1)]
+        [TestCase(int.MaxValue, int.MinValue, ExpectedResult = 1)]
+        [TestCase(0, 0, 0, ExpectedResult = 0)]
+        [TestCase(2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, ExpectedResult = 2)]
+        [TestCase(3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 43, 53, 59, ExpectedResult = 1)]
+        public int CheckFindingGCD(int num1, int num2, params int[] numbers)
+        {
+            return GreatestCommonDivisor.GetGCD(num1, num2, numbers);
         }
     }
 }

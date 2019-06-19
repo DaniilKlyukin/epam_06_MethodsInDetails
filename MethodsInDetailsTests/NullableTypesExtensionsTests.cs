7ea97938a5e45333374baffef086d240e62130ae
Daniil <anatoliikljukin@mail.ru>
2019-06-19 14:51:37 +0400

    Create NullableTypesExtensionsTests.cs
    Added tests for 3rd task.

---
+++ MethodsInDetailsTests/NullableTypesExtensionsTests.cs
@@ -0,0 +1,37 @@
+﻿using TasksLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MethodsInDetailsTests
+{
+    [TestClass]
+    class NullableTypesExtensionsTests
+    {
+        [TestMethod]
+        public void CheckIsNull()
+        {
+            int? i1 = 1;
+            Assert.AreEqual(false, i1.IsNull());
+
+            int? i2 = null;
+            Assert.AreEqual(true, i2.IsNull());
+
+            string s1 = null;
+            Assert.AreEqual(true, s1.IsNull());
+
+            string s2 = "Some str";
+            Assert.AreEqual(false, s2.IsNull());
+
+            object o1 = "boxing";
+            Assert.AreEqual(false, o1.IsNull());
+
+            object o2 = null;
+            Assert.AreEqual(true, o2.IsNull());
+
+            int[] arr1 = null;
+            Assert.AreEqual(true, arr1.IsNull());
+
+            int[] arr2 = { 1, 2, 3 };
+            Assert.AreEqual(false, arr2.IsNull());
+        }
+    }
+}

4a73fa3e53d23d8816bae2baeabb72920a4db457
Daniil <anatoliikljukin@mail.ru>
2019-06-26 16:36:17 +0400

    Update GreatestCommonDivisorTests.cs
    Removed unnecessary arguments.

--- MethodsInDetailsTests/GreatestCommonDivisorTests.cs
+++ MethodsInDetailsTests/GreatestCommonDivisorTests.cs
@@ -16,10 +16,10 @@ namespace MethodsInDetailsTests
         [TestCase(0, 0, 0, ExpectedResult = 0)]
         [TestCase(2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, ExpectedResult = 2)]
         [TestCase(3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 43, 53, 59, ExpectedResult = 1)]
-        public int CheckFindingGCDByEuclid(int num1, int num2, params int[] numbers)
+        public int CheckFindingGCDByEuclid(params int[] numbers)
         {
             double workTime;
-            return EuclidGreatestCommonDivisor.GetGCD(out workTime, num1, num2, numbers);
+            return EuclidGreatestCommonDivisor.GetGCD(out workTime, numbers);
         }
 
         [TestCase(10, 15, 20, ExpectedResult = 5)]
@@ -29,25 +29,25 @@ namespace MethodsInDetailsTests
         [TestCase(0, 0, 0, ExpectedResult = 0)]
         [TestCase(2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, ExpectedResult = 2)]
         [TestCase(3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 43, 53, 59, ExpectedResult = 1)]
-        public int CheckFindingGCDByStain(int num1, int num2, params int[] numbers)
+        public int CheckFindingGCDByStain(params int[] numbers)
         {
             double workTime;
-            return StainGreatestCommonDivisor.GetGCD(out workTime, num1, num2, numbers);
+            return StainGreatestCommonDivisor.GetGCD(out workTime, numbers);
         }
 
         [Test, TestCaseSource("GCDByStain")]
-        public void CheckFindingGCDByStainBoundary(int expected, int num1, int num2, params int[] numbers)
+        public void CheckFindingGCDByStainBoundary(int expected, params int[] numbers)
         {
             double workTime;
-            var actual = StainGreatestCommonDivisor.GetGCD(out workTime, num1, num2, numbers);
+            var actual = StainGreatestCommonDivisor.GetGCD(out workTime, numbers);
             MSUnitTest.Assert.AreEqual(expected, actual);
         }
 
         static object[] GCDByStain =
         {
-            new object[] { 1, int.MaxValue, int.MinValue,new int[] {0 } },
-            new object[] { int.MaxValue, int.MaxValue, 0,new int[] {0 }},
-            new object[] { int.MaxValue, int.MaxValue, int.MaxValue, new int[] { 0 } }
+            new object[] { 1, new int[] { int.MaxValue, int.MinValue, 0 } },
+            new object[] { int.MaxValue,new int[] {int.MaxValue, 0, 0 }},
+            new object[] { int.MaxValue, new int[] { 0, int.MaxValue, int.MaxValue } }
         };
     }
 }
